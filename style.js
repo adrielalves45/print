5a28de7d59553deb73df1956b04ccef3474fe89a
adrielalves45 <63862396+adrielalves45@users.noreply.github.com>
2020-04-17 12:06:35 -0300

    Update style.js

--- style.js
+++ style.js
@@ -1,4 +1,5 @@
 // style CSS console.log
+var inputStyle = 'color:blue';
 var print = {
 	s:function(mes) {
 		console.log("%c" + mes, "font-size: 20px; color:lightgreen");
@@ -9,5 +10,16 @@ var print = {
 	w:function(mes){console.warn(mes);
 	},
 	e:function(mes){console.error(mes);
+	},
+	i:function(mes, inputStyle){
+		if(this.inputStyle != null){
+			inputStyle = this.inputStyle;
+			console.log("%cStyle defined as: " + this.inputStyle, this.inputStyle);
+			console.log("%c" + mes, this.inputStyle);
+			
+		} else {
+			console.log("You must define a style > %cprint.i('hi', 'color:red')", inputStyle);
+		}
+				
 	}
 };
